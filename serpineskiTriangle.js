30bb93c2a6caa173ba0ef63b8f36e6131122a798
MohammadYSF <aa.yosefiyan7@gmail.com>
2023-01-20 00:53:48 +0330

    ok ing the position of triangle in serpineskiTriangle.js

--- serpineskiTriangle.js
+++ serpineskiTriangle.js
@@ -60,7 +60,7 @@ function SerpineskiTriangle(){
     this.turtle=function(){
       background(51);
       resetMatrix();
-      translate(400, height - 100);
+      translate(100, height - 100);
       stroke(255 , 150);
       strokeWeight(1);
       for (let i = 0 ; i < this.sentence.length ; i ++) {
